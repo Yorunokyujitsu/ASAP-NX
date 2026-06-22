edd0fa15eb814771ea0202b29fefacc1fef77d71
Yorunokyujitsu <164279972+Yorunokyujitsu@users.noreply.github.com>
2026-06-23 00:17:41 +0900

    Miscellaneous improvements.

--- misc/scripts/repos.sh
+++ misc/scripts/repos.sh
@@ -37,7 +37,7 @@ if [[ "${ENABLE_CUSTOM:-0}" == "1" ]]; then
     #"Yorunokyujitsu/sphaira@test"
 
     # System modules and overlays (latest libultrahand aa233a1)
-    "ppkantorski/Ultrahand-Overlay@860078e"
+    "ppkantorski/Ultrahand-Overlay@9d61cee"
     "ppkantorski/EdiZon-Overlay@769344a"
     "masagrator/FPSLocker@7b316c9"
     "ppkantorski/NX-FanControl@2580291"
