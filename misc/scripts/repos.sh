93922ed3daee0ca0450724a116c83c81ec305654
Yorunokyujitsu <164279972+Yorunokyujitsu@users.noreply.github.com>
2026-06-28 06:38:52 +0900

    Update build script and latest version.inc
    - Updated Horizon-OC commit.
    - Updated ASAP-Test version info.

--- misc/scripts/repos.sh
+++ misc/scripts/repos.sh
@@ -44,7 +44,7 @@ if [[ "${ENABLE_CUSTOM:-0}" == "1" ]]; then
     "ppkantorski/ovl-sysmodules@32f1045"
     "masagrator/ReverseNX-RT@725eb27"
     "ppkantorski/Status-Monitor-Overlay@50153ec"
-    "Yorunokyujitsu/Horizon-OC@personal" # d80d59d
+    "Yorunokyujitsu/Horizon-OC@personal" # 859b225
 
     # Homebrews
     "HamletDuFromage/aio-switch-updater=ASAP-Updater@3d38eaa"
